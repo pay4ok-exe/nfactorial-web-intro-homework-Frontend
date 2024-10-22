b3f0792c0ba7d0a71110dab72902d9433ecb3624
Akim Ospan <ospanakim05@gmail.com>
2024-10-22 19:04:59 +0500

    Added Seminar 6

---
+++ Seminar 6/planC.js
@@ -0,0 +1,6 @@
+// Создать функцию `leaveFast`, которая
+// после двух секунд должна вернуть в resolve "leave the bank in 2 seconds"
+// и после 4 секунд должна вернуть в resolve "close the road in 4 seconds"
+// Если два условия выполнены вывести в консоль "we almost did it!" и
+// переписать глобальную переменную `window.plans.planC=true;`
+
---
+++ Seminar 6/planD.js
@@ -0,0 +1,9 @@
+// Создать переменную `flashLightsInterval`
+// Создать 3 функции `changeColor`, `flashText`, `stopTextColor`
+// `changeColor` должна реагировать на нажатие кнопки `#start`
+// Также, должна создать интервал к flashLightsInterval и каждую секунду вызывать
+// коллбэк `flashText`
+// `flashText` должна менять классы для id "police" cо ".stop" на ".go" и обратно,
+// периодичностью в 1 секунду
+// `stopTextColor` должна реагировать на нажатие кнопки `#stop`
+// также, должна очистить интервал и переписать глобальную переменную `window.plans.planD=true;`
