b85aae2c339eba2dfcda5f49238d519afa68c6f0
Akim Ospan <ospanakim05@gmail.com>
2024-10-22 19:18:00 +0500

    Task1_1

--- Seminar 6/planA.js
+++ Seminar 6/planA.js
@@ -1,3 +1,13 @@
 // Создать функцию 'createPlan' которая после 5 секунд будет возвращать промис с двумя методами `.then`
 // Первый метод должен вернуть в консоль текст: "План придуман, полетели!"
 // Второй метод должен переписать глобальную переменную `window.plans.planA=true;`
+
+function createPlan() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("Plan Created"), 5000);
+  });
+}
+
+createPlan().then((message) => {
+  console.log(message);
+});
